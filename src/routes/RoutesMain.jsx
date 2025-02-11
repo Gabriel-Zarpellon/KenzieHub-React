11d19f1cec8cb656038c4b1d454c9af18bfff9e8
Gabriel Zarpellon <gabriel_zv@hotmail.com>
2025-02-11 12:44:48 -0300

    feat: create protected route for dashboard page

--- src/routes/RoutesMain.jsx
+++ src/routes/RoutesMain.jsx
@@ -2,13 +2,16 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../pages/LoginPage";
 import { DashboardPage } from "../pages/DashboardPage";
 import { RegisterPage } from "../pages/RegisterPage";
+import { ProtectedRoutes } from "./ProtectedRoutes";
 
 export function RoutesMain() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/dashboard" element={<ProtectedRoutes />}>
+        <Route index element={<DashboardPage />} />
+      </Route>
     </Routes>
   );
 }
